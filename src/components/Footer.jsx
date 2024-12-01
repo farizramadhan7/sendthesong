fe78f928b595cccf13675506d13dca5b49b4dfa5
unknown <farizrizqullah6@gmail.com>
2024-12-01 07:54:08 +0700

    footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
   return (
-    <div className ="mt-2">
-    <div className ="text-center font-semibold text-lg text-gray-500 hover:text-black"style={{ fontFamily: 'ReenieBeanie' }}>@farizramaadhan</div>
+    <div className="mt-2 flex justify-center items-end h-full">
+      <a 
+        href="https://www.instagram.com/farizramaadhan/" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="font-semibold text-lg text-gray-500 hover:text-black" 
+        style={{ fontFamily: 'ReenieBeanie' }}
+      >
+        @farizramaadhan
+      </a>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
